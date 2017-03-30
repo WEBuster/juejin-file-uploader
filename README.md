33d2faddd2659089ad7c4a5aea058d701fcc6cf6
yangtao <yangtao@xitu.io>
2017-03-30 17:46:46 +0800

    file upload extend

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
 # qiniu-file-uploader
- 依照七牛上传资源流程开发的一个web端上传文件的插件，
+ web端上传资源到cdn的插件（ 依照七牛上传资源流程）
+
+## 安装使用
+```
+npm install juejin-file-upload
+
+```
+
+```html
+<template>
+  <div>
+    <input type="file" @change="postFile($event.target.files[0])">
+  </div>
+</template>
+
+<script>
+import Uploader from 'juejin-file-uploader'
+
+export default {
+  methods: {
+    postFile (_file) {
+      let uploader = new Uploader({
+        bucket: 'assets',
+        tokenUrl: 'tokenUrl',
+        uploadUrl: 'uploadUrl'
+      })
+      uploader.upload(_file)
+        .then(result => {
+          console.log(result)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  }
+}
+</script>
+```
+
+## 参数
+```
+var upload = new Uploader({
+    "bucket": "bucket" //存储空间
+    "uploadUrl": "uploadUrl" //上传文件至七牛云的接口地址
+    "tokenUrl": "tokenUrl" //获取token的接口地址(juejin.im封装的接口)
+  })
+```
+## 参数及接口流程说明
+
+七牛云资源上传需要正确的token,key及formData才能上传成功，
+
+而（token,key）的获取是需要（Access_Key,Secret_Key,bucket）通过 SDK取得的，
+
+**tokenUrl** 接口就是用来取（token,key）的。
+
+同时tokenUrl需要接收<bucket> 和<fileName> 参数；
+
+    ps: <fileName>是插件根据选择的文件自动获取的,不用手动设置,只需配置<bucket>
+
+**tokenUrl**接口响应后会返回含有token,key,url的json
+
+
+插件会将这个json里面的（token,key）连同fileName（input选择的文件）一并传给七牛云接口 **uploadUrl**
+
+如果文件上传成功, 插件会将**tokenUrl**返回的url当做参数resolve,
+
+    ps: tokenUrl响应接口中的url是如果资源上传成功储存的位置
+
+
---
+++ dist/uploader.js
@@ -0,0 +1,142 @@
+/**
+ * Uploader v1.0.0
+ * (c) 2017 yangtao <iam_yt@163.com>
+ * @license MIT
+ *
+ */
+
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() : typeof define === 'function' && define.amd ? define(factory) : (global.Uploader = factory())
+}(this, function () {
+  'use strict'
+  /** Uploader
+   * @param {String} bucket  存储空间
+   * @param {String} uploadUrl 上传文件的接口地址
+   * @param {String} tokenUrl 获取token的接口地址
+   * @return {Object} Promise类型,返回结果一个url对象({http,https})
+   */
+  var Uploader = function (options) {
+    var _options = {
+      bucket: null,
+      uploadUrl: '',
+      tokenUrl: ''
+    }
+    Object.assign(_options, options || {})
+    this._options = _options
+    this._file = {}
+  }
+
+  // 上传
+  Uploader.prototype.upload = function (_file) {
+    if (!_file) {
+      return
+    }
+    this._file = _file
+
+    return new Promise((resolve, reject) => {
+      resolve(this.getToken())
+    }).then(rst => {
+      if (+rst.s !== 1) {
+        return Promise.reject(new Error('token 错误'))
+      } else {
+        return this.uploadFile(rst.d)
+      }
+    })
+  }
+
+  /**
+   * @return {Object} 获取token & 上传的图片的路径等信息
+   */
+  Uploader.prototype.getToken = function () {
+    var tokenUrl = this._options.tokenUrl
+    var bucket = this._options.bucket
+    var urlStr = toQueryString({ bucket: bucket, filename: this._file.name })
+    return new Promise((resolve, reject) => {
+      var xhr = new XMLHttpRequest()
+      xhr.open('GET', tokenUrl + '?' + urlStr, true)
+      xhr.responseType = 'json'
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState !== 4) {
+          return
+        }
+        if (xhr.status == 200) {
+          var blkRet = xhr.response
+          resolve(blkRet)
+        } else {
+          reject(new Error(xhr.statusText))
+        }
+      }
+      xhr.onerror = function () {
+        reject(new Error(xhr.statusText))
+      }
+      xhr.send(null)
+    })
+  }
+
+  /**
+   * @param {String} token
+   * @param {String} key
+   * @param {Object} url
+   * @return {Object} 成功返回 url地址({http,https})
+   */
+  Uploader.prototype.uploadFile = function ({ token, key, url }) {
+    var self = this
+    var file = this._file
+    var uploadUrl = this._options.uploadUrl
+    return new Promise((resolve, reject) => {
+      var xhr = new XMLHttpRequest()
+      xhr.open('POST', uploadUrl, true)
+      var formData
+      formData = new FormData()
+      formData.append('token', token)
+      formData.append('file', file)
+      formData.append('key', key)
+      xhr.responseType = 'json'
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState !== 4) {
+          return
+        }
+        if (xhr.status == 200) {
+          var blkRet = xhr.response
+          resolve(url)
+        } else {
+          reject(new Error(xhr.statusText))
+        }
+      }
+      xhr.onerror = function () {
+        reject(new Error(xhr.statusText))
+      }
+      xhr.send(formData)
+    })
+  }
+
+  // util
+  function toQueryPair(key, value) {
+    if (typeof value == 'undefined') {
+      return key
+    }
+    return key + '=' + encodeURIComponent(value === null ? '' : String(value))
+  }
+  // JSON 转 URL
+  // toQueryString({bucket: 'abc',filename: 'name.jpg'}) //name=xesam&age=24
+  function toQueryString(obj) {
+    var ret = []
+    for (var key in obj) {
+      key = encodeURIComponent(key)
+      var values = obj[key]
+      if (values && values.constructor == Array) {
+        var queryValues = []
+        for (var i = 0, len = values.length, value; i < len; i++) {
+          value = values[i]
+          queryValues.push(toQueryPair(key, value))
+        }
+        ret = ret.concat(queryValues)
+      } else {
+        ret.push(toQueryPair(key, values))
+      }
+    }
+    return ret.join('&')
+  }
+
+  return Uploader
+}))
